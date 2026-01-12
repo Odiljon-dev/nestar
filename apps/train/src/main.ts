8f37774c7aaa306ab29c50a01d0ead0b65cab182
Odiljon <odiljonzokirov551@gmail.com>
2026-01-13 05:43:32 +0900

    feat: MIT TASK_ZQ working

--- apps/train/src/main.ts
+++ apps/train/src/main.ts
@@ -81,19 +81,38 @@ console.log(areArraysEqual([1, 2, 3], [4, 1, 2]));
 //////////////////////////////////////////////////////
 // TASK
 function groupAnagrams(str) {
-  const map = new Map();
+	const map = new Map();
 
-  for (let word of str) {
-    const key = word.split('').sort().join('');
+	for (let word of str) {
+		const key = word.split('').sort().join('');
 
-    if (!map.has(key)) {
-      map.set(key, []);
-    }
+		if (!map.has(key)) {
+			map.set(key, []);
+		}
 
-    map.get(key).push(word);
-  }
+		map.get(key).push(word);
+	}
 
-  return Array.from(map.values());
+	return Array.from(map.values());
 }
 
-console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
+////////////////////////////////////////////////////////////////////
+function findDuplicates(array) {
+	const map = new Map();
+	const result = [];
+
+	for (const num of array) {
+		map.set(num, (map.get(num) || 0) + 1);
+	}
+
+	for (const [key, value] of map) {
+		if (value > 1) {
+			result.push(key);
+		}
+	}
+
+	return result;
+}
+
+console.log(findDuplicates([1, 2, 3, 4, 5, 4, 3, 4]));
