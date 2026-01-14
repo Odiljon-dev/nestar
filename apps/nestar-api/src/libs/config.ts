46f72d02649fa66c964da415f051092130eee26e
Odiljon <odiljonzokirov551@gmail.com>
2026-01-14 14:25:48 +0900

    feat: develop admin related boardArticle GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -13,6 +13,8 @@ export const avilablePropertySorts = [
 	'propertyPrice',
 ];
 
+export const avilableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
+
 /** IMAGE CONFIGURATION (config.js) **/
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
