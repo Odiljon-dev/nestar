b5d42885f996c9a2c8a508e2888d75af3bb86386
Odiljon <odiljonzokirov551@gmail.com>
2026-01-26 17:17:08 +0900

    feat: create WebSocket Connection process

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -8,6 +8,7 @@ import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 import { T } from './types/common';
+import { SocketModule } from './socket/socket.module';
 
 // Module markaziy module hisoblanadi
 /** Bu yerda PROPERTYLAR birlashtirib turadi  **/
@@ -35,7 +36,7 @@ import { T } from './types/common';
 		}),
 		// COMPONENT HAMDA DATABASE MODULENI ROUTING QILMAGANIMIZNI SABABI BU BIZNING FILE MODULE BO'LGANI UCHUN
 		ComponentsModule, // HTTP barcha mantiqlarni bitta joyda saqlaydi!
-		DatabaseModule, // TCP connection bolgani uchun!
+		DatabaseModule, SocketModule, // TCP connection bolgani uchun!
 	],
 	controllers: [AppController], //RestApi!
 	providers: [AppService, AppResolver], //GraphQl-Api!
--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './interceptor/Logging.interceptor';
 import { graphqlUploadExpress } from 'graphql-upload';
 import * as express from 'express';
+import { WsAdapter } from '@nestjs/platform-ws';
 /** Standard mode => loyihamizda 1 ta server ishlatadigan bo'lsak **/
 /** Monorepo module bir nechta serverlarni bitta repositerda saqlanadi **/
 /** NestJS ni ingrinendlari => RESOLVER & MODULES & SERVIECES & GUARDS & PIPES & INTERCEPTOR**/
@@ -23,6 +24,8 @@ async function bootstrap() {
 	// EXPRESS
 	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 })); // Maximalni faylarimizni o'lchovi hamda  qancha bo'lishini hosil qilganmiz
 	app.use('/uploads', express.static('./uploads')); // ./uploads faylimizni tashqi olamga ochiqlaganmiz
+
+	app.useWebSocketAdapter(new WsAdapter(app));
 
 	//  BU EXPRESSNI USTIGA QURGANIMIZ UCHUN BU YERDA HAM ISHLATYABMIZ
 	await app.listen(process.env.PORT_API ?? 3000); // PORT_API orqali 3007 portda ishga tushiryapmiz agar mavjud bo'lmasa 3000 portda ishga tushadi
